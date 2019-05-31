ebfd18d3b1ae44f585f96a67b03829e228df4757
Roosevelt Villela <rmvillela@outlook.com>
2019-05-31 11:40:00 -0300

    WebpackDevServer, DefaultProps e PropTypes

--- aulas/modulo1/src/index.js
+++ aulas/modulo1/src/index.js
@@ -1,1 +1,30 @@
-alert('hello');
+import React from 'react';
+import PropTypes from 'prop-types';
+import { render } from 'react-dom';
+
+class Button extends React.Component {
+  render() {
+    return (
+      <a href="#" onClick={this.props.onClick}>
+        {this.props.children}
+      </a>
+    )
+  }
+}
+
+Button.defaultProps = {
+  children: "Salvar"
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.string
+};
+
+class App extends React.Component {
+  render() {
+    return <Button onClick={() => { alert("button 1"); }}/>;
+  }
+}
+
+render(<App />, document.getElementById('app'));
